a383628f81be67b2610119b15f0c4f21a450981d
Hieu Nguyen <macbookM1@macs-MacBook-Air.local>
2023-06-07 14:54:05 +0700

    migrate: nx to 16.3.2

--- packages/rpc/src/migration/0001_fair_beyonder.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `user_answer` DROP FOREIGN KEY `user_answer_question_id_security_question_id_fk`;
-
-DROP INDEX `email_idx` ON `subscriber`;
-CREATE INDEX `activity_user_id_idx` ON `user_activity` (`user_id`);
-CREATE INDEX `activity_action_idx` ON `user_activity` (`action`);
-CREATE INDEX `email_idx` ON `subscriber` (`email`);
