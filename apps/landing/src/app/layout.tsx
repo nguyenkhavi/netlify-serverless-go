8a6dd99cc21914223d62e49170d85b47248ca7ec
Teddy <vu.dinh@sens-vn.com>
2023-06-09 13:27:50 +0700

    fix: build

--- apps/landing/src/app/layout.tsx
+++ apps/landing/src/app/layout.tsx
@@ -2,6 +2,7 @@
 import { Metadata } from 'next';
 import classcat from 'classcat';
 import { Archivo } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
 //LAYOUT, COMPONENTS
 import Header from '_@landing/layouts/Header';
 import Footer from '_@landing/layouts/footer/Footer';
@@ -9,6 +10,7 @@ import BaseToast from '_@shared/components/toast/BaseToast';
 import ModalFeedback from '_@landing/components/modal/ModalFeedback';
 //RELATIVE MODULES
 import './globals.css';
+import ClientProvider from './provider/ClientProvider';
 
 export const metadata: Metadata = {
   title: 'Fleamint',
@@ -22,14 +24,19 @@ const archivo = Archivo({
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={classcat([archivo.variable, 'hidden-scrollbar'])}>
-      <body>
-        <Header />
-        {children}
-        <Footer />
-        <ModalFeedback />
-        <BaseToast />
-      </body>
-    </html>
+    <ClerkProvider>
+      <ClientProvider>
+        <html lang="en" className={classcat([archivo.variable, 'hidden-scrollbar'])}>
+          <body>
+            <Header />
+            {/* <SessionProvider>{children}</SessionProvider> */}
+            {children}
+            <Footer />
+            <ModalFeedback />
+            <BaseToast />
+          </body>
+        </html>
+      </ClientProvider>
+    </ClerkProvider>
   );
 }
