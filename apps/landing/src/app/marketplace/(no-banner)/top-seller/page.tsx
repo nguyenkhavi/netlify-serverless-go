3558ba01dcca64acaef3ace45a35741a98ad9099
Lam Nguyen Thanh <lam.nguyen@sens-vn.com>
2023-06-29 09:34:42 +0700

    bug: Fix get data error in seller page

--- apps/landing/src/app/marketplace/(no-banner)/top-seller/page.tsx
+++ apps/landing/src/app/marketplace/(no-banner)/top-seller/page.tsx
@@ -6,6 +6,7 @@ import HomeAdvHorizontal from '_@landing/app/comps/HomeAdvHorizontal';
 import { useIndexedDBContext } from '_@landing/app/provider/IndexedDBProvider';
 //LAYOUT, COMPONENTS
 import SellerCard from '_@landing/components/card/SellerCard';
+import { SkeCard } from '_@landing/components/skeleton/skeleton';
 import ContentWithBack from '_@landing/components/content-box/ContentWithBack';
 //SHARED
 
@@ -13,17 +14,21 @@ export default function TopSellerPage() {
   const { bestSeller } = useIndexedDBContext();
   return (
     <ContentWithBack title="Top Sellers" path="/marketplace">
-      {bestSeller.map((item, index) => (
-        <React.Fragment key={index}>
-          <SellerCard value={item} view="full" />
-          {(index + 1) % 4 === 0 ? (
-            <HomeAdvHorizontal
-              className={classcat(['col-span-full', index + 1 === 4 ? 'md:hidden' : ''])}
-              isHome={false}
-            />
-          ) : null}
-        </React.Fragment>
-      ))}
+      {bestSeller.loading
+        ? Array(8)
+            .fill(1)
+            .map((item, index) => <SkeCard key={index} />)
+        : bestSeller.data.map((item, index) => (
+            <React.Fragment key={index}>
+              <SellerCard value={item} view="full" />
+              {(index + 1) % 4 === 0 ? (
+                <HomeAdvHorizontal
+                  className={classcat(['col-span-full', index + 1 === 4 ? 'md:hidden' : ''])}
+                  isHome={false}
+                />
+              ) : null}
+            </React.Fragment>
+          ))}
     </ContentWithBack>
   );
 }
